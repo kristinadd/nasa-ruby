b1652745a343426bdeb5bce3335da5363f6a9d3e
kristinadd <kristina@productboard.com>
2025-03-08 19:36:25 +0100

    Faraday

---
+++ Faraday/nasa_client.rb
@@ -0,0 +1,42 @@
+require 'faraday'
+require 'json'
+
+class NasaClient
+  BASE_URL = 'https://api.nasa.gov/planetary/apod'
+
+  def initialize(api_key:)
+    @api_key = api_key
+    @conn = Faraday.new(url: BASE_URL) do |faraday|
+      # Use the default adapter (Net::HTTP)
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_image(date: nil, hd: false)
+    query_params = {
+      'api_key' => @api_key,
+      'hd' => hd
+    }
+    query_params['date'] = date if date
+
+    response = @conn.get do |req|
+      req.params = query_params
+    end
+
+    if response.success?
+      JSON.parse(response.body)
+    else
+      { "error" => "Request failed with status #{response.status}: #{response.body}" }
+    end
+  end
+end
+
+# Example usage:
+if __FILE__ == $0
+  client = NasaClient.new(api_key: 'API_KEY')
+  today_image = client.get_image
+  puts "Today's APOD:\n#{today_image}\n\n"
+
+  dated_image = client.get_image(date: '2012-07-07', hd: true)
+  puts "APOD for 2012-07-07:\n#{dated_image}\n\n"
+end
--- HTTParty/nasa_client.rb
+++ HTTParty/nasa_client.rb
@@ -19,13 +19,13 @@ class NasaClient
     # this adds the date parameter to the query_params hash if a date is provided
     # all modify the hash 'in place'
     query_params[:date] = date if date # modifies in place and returns the assigned value
-    query_params.store(:date, date) if date # modifies in place and assigned value
-    query_params.merge!(date: date) if date # modifies in place and returns the modified hash
-    query_params.update(date: date) if date # modifies in place and returns the modified hash
+    # query_params.store(:date, date) if date # modifies in place and assigned value
+    # query_params.merge!(date: date) if date # modifies in place and returns the modified hash
+    # query_params.update(date: date) if date # modifies in place and returns the modified hash
 
-
-    
     response = self.class.get('', query: query_params)
+
+    # puts response.inspect
 
     if response.code == 200
       JSON.parse(response.body)
